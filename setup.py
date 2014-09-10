756b5904c4181b08b153f06fbdbb3be33b69b11f
David Black <dblack@atlassian.com>
2014-09-10 11:09:07 +1000

    Add platform, license and classifiers to setup.py.
    Signed-off-by: David Black <dblack@atlassian.com>

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import os
 def read(fname):
 	return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name='django_xss_detection',
-	author='david black',
+setup(name='django-xss-detection',
+	author='David Black',
 	author_email='dblack@atlassian.com',
 	url='https://bitbucket.org/atlassian/django_xss_detection',
 	packages=find_packages(),
@@ -18,6 +18,20 @@ setup(name='django_xss_detection',
 		'Django>=1.5',
 		'lxml',
 	],
-
+    platforms=['any'],
+    license='BSD',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: BSD License',
+        'Framework :: Django',
+    ],
 )
 
